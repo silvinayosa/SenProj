93d70e7d60c3d53a1c9afadb36de3ab80b1c1d16
AndersonDaniel010 <107477864+AndersonDaniel010@users.noreply.github.com>
2024-05-30 02:04:41 +0800

    Analysis page

--- frontend/green-horizon-app/app/layout.tsx
+++ frontend/green-horizon-app/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
 import "./globals.css";
+import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
