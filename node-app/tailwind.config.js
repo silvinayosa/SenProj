6a52aa2aa13d5722719f8a19c29074e8b56cf022
AndersonDaniel010 <107477864+AndersonDaniel010@users.noreply.github.com>
2024-11-05 18:38:56 +0800

    homepage

--- node-app/tailwind.config.js
+++ node-app/tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./views/**/*.{html,js,ejs}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        animation: {
+          blob: "blob 3s infinite",
+        },
+        blob: {
+          "0%, 100%": {
+            transform: "scale(1)",
+          },
+          "50%": {
+            transform: "scale(1.4)",
+          },
+
+          "66%": {
+            transform: "scale(1.2)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+
+    },
   },
   plugins: [],
 }
