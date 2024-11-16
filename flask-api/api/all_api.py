924e912b8e99ff7da39a7d3138658e02f8f87070
AndersonDaniel010 <107477864+AndersonDaniel010@users.noreply.github.com>
2024-11-16 23:19:57 +0800

    create event

--- flask-api/api/all_api.py
+++ flask-api/api/all_api.py
@@ -93,7 +93,7 @@ def connect_to_db():
     conn = sqlite3.connect("../database/SeniorProject.db", check_same_thread=False)
     return conn
 
-db_path = '../database/SeniorProject.db'  # Update the path to match your setup
+db_path = '../database/SeniorProject3.db'  # Update the path to match your setup
 
 def get_db_connection():
     conn = sqlite3.connect(db_path)
